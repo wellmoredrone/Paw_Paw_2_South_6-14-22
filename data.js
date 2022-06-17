7f93f4193ef5a91362ca6865b618e8268847e2a0
wellmoredrone <85592388+wellmoredrone@users.noreply.github.com>
2022-06-17 07:30:37 -0400

    init

---
+++ data.js
@@ -0,0 +1,57 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pp2-south-6-14-22",
+      "name": "PP2 South 6-14-22",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5500,
+      "initialViewParameters": {
+        "yaw": 0.10965725119201686,
+        "pitch": 0.46959708299605296,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.5720742650647175,
+          "pitch": 0.3802643004279318,
+          "title": "Belt Line Foundation",
+          "text": "Belt Line Foundation is being constructed here."
+        }
+      ]
+    }
+  ],
+  "name": "Paw Paw 2 South 6-14-22",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
